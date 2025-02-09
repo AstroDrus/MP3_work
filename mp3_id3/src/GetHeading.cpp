8cee0ea3febe9ad215a2bc1f5ece699fb79cdd38
AndreyK <andevil95@mail.ru>
2025-02-09 19:55:14 +0300

    Reworked func which shows real size of MP3. Reworked adding version and flag in private fields

--- mp3_id3/src/GetHeading.cpp
+++ mp3_id3/src/GetHeading.cpp
@@ -5,8 +5,6 @@
 #include <cstdint>
 #include <vector>
 #include <algorithm>
-#include <bitset>
-#include <climits>
 
 #include <arpa/inet.h>
 
@@ -24,7 +22,7 @@ ID3 GetHeading( std::ifstream& source_file)
     //verification if file can be opened  
     if (!source_file.is_open())
     {
-    throw std::invalid_argument("Incorrect source file name!"); 
+        throw std::invalid_argument("Incorrect source file name!"); 
     }
     // amount of bayts in head of MP3_file
     static constexpr const int sizeID3 = 3;
@@ -35,135 +33,92 @@ ID3 GetHeading( std::ifstream& source_file)
     {
         throw std::invalid_argument("source file is not an MP3 file with IDv3 tag");
     }
-     ID3 id3{};
-    
+        
     // besides size_
     constexpr int size_of_elements = 3;
-    std::uint8_t* array[size_of_elements] = 
-    { 
-          id3.get_adr_version_()
-        , id3.get_adr_sub_version_()
-        , id3.get_adr_flag_()
-    };
-    
+
+    std::uint8_t array[ size_of_elements ] = { 0 };
+
     for (int i = 0; i < size_of_elements; ++i) 
     {
-        source_file.read( reinterpret_cast< char* >( array[ i ] ), sizeof( *( array[ i ] ) ) );
+        source_file.read( reinterpret_cast< char* >( &array[ i ] ), sizeof( ( array[ i ] ) ) );
     }
 
-    source_file.read( reinterpret_cast< char* >( id3.get_adr_size_() ), sizeof( *id3.get_adr_size_() ) );
+    ID3 id3{};
+    id3.set_version_( array[0] );
+    id3.set_sub_version_( array[1] ); 
+    id3.set_flag_( array[2] );
 
-    id3.set_size_(id3.make_right_sizeadr(*id3.get_adr_size_()));
+    std::uint32_t buffer_size;
+    source_file.read( reinterpret_cast< char* >( &buffer_size ), sizeof( buffer_size) );
+    id3.set_size_( buffer_size );
+    id3.set_size_(id3.make_right_sizeadr(id3.get_size_()));
 
 return id3;
-
 }
 
-std::uint32_t ID3::make_right_sizeadr(uint32_t size)
+std::uint32_t ID3::make_right_sizeadr( std::uint32_t size )
 {
-    size = ntohl(size);
-    //int buffer_size = 0xFFFFF;
-    int buffer_size = size;
-   // std::cout << "bits of size before magic " << std::bitset<sizeof(size) * CHAR_BIT>(size) << std::endl;
+ size = ntohl(size);
+ 
+		constexpr static std::uint32_t mask = 0x7F;
+		
+		std::uint32_t prevMask{0};
+		for( std::size_t byte = 1; byte < sizeof( std::uint32_t ); ++byte )
+		{
+       const std::uint32_t crntByteMask = (( prevMask << 7 ) | mask);
+       const std::uint32_t oldButGold = ( size & crntByteMask );
+       const std::uint32_t shifted = size >> 1;
+       const std::uint32_t newValue = ( shifted & ~( crntByteMask ) );
+       
+       size = ( (oldButGold) | newValue );
+       
+       prevMask = crntByteMask;
+		}
 
-//todo make cycle
-    unsigned int first_byte = buffer_size % 128; // 1000 0000 - take last 2 symbols
-  // std::cout << "first byte " << std::hex << first_byte << " " << "bits of first byte " << std::bitset<sizeof(first_byte) * CHAR_BIT>(first_byte) << std::endl;
-    buffer_size = buffer_size >> 8;
-   // std::cout << "buffer_size after making first_byte " << std::hex << buffer_size << std::endl;
-
-    unsigned int second_byte = buffer_size % 128;
-   // std::cout << "second byte " << std::hex << second_byte << " " << "bits of second byte " <<std::bitset<sizeof(second_byte) * CHAR_BIT>(second_byte) << std::endl;
-    buffer_size = buffer_size >> 8;
-   // std::cout << "buffer_size after making second_byte " << std::hex << buffer_size << std::endl;
-
-    unsigned int third_byte = buffer_size % 128;
-    // std::cout << "third byte " << std::hex << third_byte << " " << "bits of third byte " << std::bitset<sizeof(third_byte) * CHAR_BIT>(third_byte) << std::endl;
-
-
-
-    if ((second_byte & 0x1) == 0x1)
-    {
-       first_byte = first_byte | (1u << 7);
-    }
-    else
-    {
-        first_byte = first_byte | (0u << 7);
-    }
-    second_byte = second_byte >> 1;
-
-    if ((third_byte & 0x3) == 0x1)
-    {
-       second_byte = second_byte | (1u << 6);
-       second_byte = second_byte | (0u << 7);
-    }
-
-    else if ((third_byte & 0x3) == 0x2)
-    {
-       second_byte = second_byte | (0u << 6);
-       second_byte = second_byte | (1u << 7);
-    }
-
-    else if ((third_byte & 0x3) == 0x3)
-    {
-       second_byte = second_byte | (1u << 6);
-       second_byte = second_byte | (1u << 7);
-    }
-
-    third_byte = third_byte >> 2;
-
-    int result = 0;
-    result = third_byte;
-    result = result << 8;
-    result += second_byte;
-    result = result << 8;
-    result += first_byte;
-
-    //std::cout << "result in 16th " <<std::hex << result << " result in bits " << std::bitset<sizeof(result) * CHAR_BIT>(result) << std::endl;
-
-    return result;
+return size;
 }
 
 //getters
-std::uint8_t* ID3::get_adr_version_()
+std::uint8_t ID3::get_version_()
 {
-    return &version_;
+		return version_;
 } 
 
-std::uint8_t* ID3::get_adr_sub_version_()
+std::uint8_t ID3::get_sub_version_()
 {
-    return &sub_version_;
+		return sub_version_;
 } 
 
-std::uint8_t* ID3::get_adr_flag_()
+std::uint8_t ID3::get_flag_()
 {
-    return &flag_;
+		return flag_;
 } 
 
-std::uint32_t* ID3::get_adr_size_()
+std::uint32_t ID3::get_size_()
 {
-    return &size_;
+		return size_;
 } 
 
 //setters
 void ID3::set_version_(const std::uint8_t val)
 {
-    version_ = val;
+		version_ = val;
 }
 
 void ID3::set_sub_version_(const std::uint8_t val)
 {
-    sub_version_ = val;
+		sub_version_ = val;
 }
 
 void ID3::set_flag_(const std::uint8_t val)
 {
-   flag_ = val;
+	 flag_ = val;
 }
 
 void ID3::set_size_(const std::uint32_t val)
 {
-   size_ = val;
+	 size_ = val;
 }
 
 } //namespace MP3
